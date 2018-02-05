c0ba20b86d5c546f48e30ef92f79e6f6a14b2d41
melancholaique <melancholique655312@gmail.com>
2018-02-05 22:49:22 +0000

    created app skeleton

---
+++ todoapp/routes.py
@@ -0,0 +1,8 @@
+from todoapp import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+        return render_template('index.html',
+                               title='todo this page')
